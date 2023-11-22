aa66137357d8c57de3f49bd0d03e356dc7c07bb8
TOBENTOch <info@tobento.ch>
2023-11-22 19:33:36 +0100

    removed docblock info

--- src/QueuesInterface.php
+++ src/QueuesInterface.php
@@ -31,7 +31,7 @@ interface QueuesInterface
      *
      * @param string $name
      * @return null|QueueInterface
-     * @throws QueueException If queue does not exist
+     * @throws QueueException
      */
     public function get(string $name): null|QueueInterface;
     
