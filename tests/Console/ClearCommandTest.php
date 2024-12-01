b2a03cd0eb17c4feeccc69196d53c9ad70537a33
TOBENTOch <info@tobento.ch>
2024-12-01 07:47:06 +0100

    test added that unique jobs cache item gets cleared

--- tests/Console/ClearCommandTest.php
+++ tests/Console/ClearCommandTest.php
@@ -13,9 +13,12 @@ declare(strict_types=1);
 
 namespace Tobento\Service\Queue\Test\Console;
 
+use Psr\SimpleCache\CacheInterface;
 use PHPUnit\Framework\TestCase;
 use Tobento\Service\Console\Test\TestCommand;
 use Tobento\Service\Queue\Console\ClearCommand;
+use Tobento\Service\Queue\Parameter\Unique;
+use Tobento\Service\Queue\Test\Helper;
 use Tobento\Service\Queue\Test\Mock;
 use Tobento\Service\Queue\JobProcessor;
 use Tobento\Service\Queue\Job;
@@ -73,6 +76,37 @@ class ClearCommandTest extends TestCase
         ->doesntExpectOutput('Jobs cleared from queue secondary')
         ->expectsExitCode(0)
         ->execute($container);
+    }
+    
+    public function testClearsCacheItemsFromUniqueJobs()
+    {
+        $container = new Container();
+        $cache = Helper::createCache();
+        $container->set(CacheInterface::class, $cache);
+
+        $queues = new Queues(
+            new InMemoryQueue(name: 'primary', jobProcessor: new JobProcessor($container)),
+            new InMemoryQueue(name: 'secondary', jobProcessor: new JobProcessor($container)),
+        );
+        
+        $this->assertFalse($cache->has('job-unique:foo'));
+        $this->assertFalse($cache->has('job-unique:BAR'));
+        
+        $queues->queue('primary')->push((new Mock\CallableJob(id: 'foo'))->parameter(new Unique()));
+        $queues->queue('secondary')->push((new Mock\CallableJob(id: 'bar'))->parameter(new Unique(id: 'BAR')));
+        $container->set(QueuesInterface::class, $queues);
+        
+        $this->assertTrue($cache->has('job-unique:foo'));
+        $this->assertTrue($cache->has('job-unique:BAR'));
+        
+        (new TestCommand(command: ClearCommand::class))
+            ->expectsOutput('Jobs cleared from queue primary')
+            ->expectsOutput('Jobs cleared from queue secondary')
+            ->expectsExitCode(0)
+            ->execute($container);
+        
+        $this->assertFalse($cache->has('job-unique:foo'));
+        $this->assertFalse($cache->has('job-unique:BAR'));
     }
     
     public function testQueueNotFound()
